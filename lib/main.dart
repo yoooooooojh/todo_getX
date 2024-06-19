5ecb4ce28543cdac2fdbd4f938dbcce905f4e052
yoooooooojh <yoooooooojh@gmail.com>
2024-06-19 19:34:29 +0900

    modules, providers, repository, storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:todo_getx/app/data/services/storage/services.dart';
 import 'package:todo_getx/app/modules/home/view.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
-void main() {
+
+void main() async {
+  await GetStorage.init();
+  await Get.putAsync(() => StorageService().init());
   runApp(const MyApp());
 }
 
