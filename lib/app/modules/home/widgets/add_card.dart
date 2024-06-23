d645dc4ca518fb0f39ab6ff530783571841b0868
yoooooooojh <yoooooooojh@gmail.com>
2024-06-23 14:05:34 +0900

    add card

--- lib/app/modules/home/widgets/add_card.dart
+++ lib/app/modules/home/widgets/add_card.dart
@@ -1,7 +1,9 @@
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
 import 'package:todo_getx/app/core/utils/extensions.dart';
+import 'package:todo_getx/app/data/models/task.dart';
 import 'package:todo_getx/app/modules/home/controller.dart';
 import 'package:todo_getx/app/widgets/icons.dart';
 
@@ -23,6 +25,7 @@ class AddCard extends StatelessWidget {
             titlePadding: EdgeInsets.symmetric(vertical: 5.0.wp),
             radius: 5,
             title: 'Task Type',
+            backgroundColor: Colors.white,
             content: Form(
               key: homeCtrl.formKey,
               child: Column(
@@ -54,6 +57,13 @@ class AddCard extends StatelessWidget {
                                   selectedColor: Colors.grey[200],
                                   pressElevation: 0,
                                   backgroundColor: Colors.white,
+                                  showCheckmark: false,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(30),
+                                  ),
+                                  side: BorderSide.none,
+                                  padding: EdgeInsets.symmetric(
+                                      vertical: 1.0.wp, horizontal: 2.0.wp),
                                   label: e,
                                   selected: homeCtrl.chipIndex.value == index,
                                   onSelected: (bool selected) {
@@ -65,10 +75,37 @@ class AddCard extends StatelessWidget {
                           .toList(),
                     ),
                   ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      minimumSize: const Size(150, 40),
+                    ),
+                    onPressed: () {
+                      if (homeCtrl.formKey.currentState!.validate()) {
+                        int icon =
+                            icons[homeCtrl.chipIndex.value].icon!.codePoint;
+                        String color =
+                            icons[homeCtrl.chipIndex.value].color!.toHex();
+                        var task = Task(
+                            title: homeCtrl.editCtrl.text,
+                            icon: icon,
+                            color: color);
+                        Get.back();
+                        homeCtrl.addTask(task)
+                            ? EasyLoading.showSuccess('Create Sucess')
+                            : EasyLoading.showError('Duplicated Task');
+                      }
+                    },
+                    child: const Text('Confirm'),
+                  ),
                 ],
               ),
             ),
           );
+          homeCtrl.editCtrl.clear();
+          homeCtrl.changeChipIndex(0);
         },
         child: DottedBorder(
           color: Colors.grey[400]!,
